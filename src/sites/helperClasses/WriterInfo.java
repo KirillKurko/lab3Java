eb174b53b19d188228c62e82dd6a933f234a6067
Кирилл Курко <kirillkurko2609@gmail.com>
2020-09-06 19:36:56 +0300

    WtriterInfo methods to print infomation about site implemented

--- src/sites/helperClasses/WriterInfo.java
+++ src/sites/helperClasses/WriterInfo.java
@@ -1,4 +1,19 @@
 package sites.helperClasses;
 
+import sites.siteClasses.Site;
+
 public class WriterInfo {
+
+    public static void printSiteName(Site site) {
+        System.out.println("Name: " + site.getName());
+    }
+
+    public static void printSiteAdvertising(Site site) {
+        System.out.println("Advertising: " + site.getAdvertising());
+    }
+
+    public static void printSiteInformation(Site site) {
+        printSiteName(site);
+        printSiteAdvertising(site);
+    }
 }
