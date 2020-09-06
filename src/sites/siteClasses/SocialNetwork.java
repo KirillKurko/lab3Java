ba13878d3314ad44db3c6527a5e7dcc72f274009
Кирилл Курко <kirillkurko2609@gmail.com>
2020-09-06 21:13:00 +0300

    SocialNetwork class implemented

--- src/sites/siteClasses/SocialNetwork.java
+++ src/sites/siteClasses/SocialNetwork.java
@@ -1,4 +1,51 @@
 package sites.siteClasses;
 
-public class SocialNetwork {
+public class SocialNetwork extends Site {
+
+    private int usersAmount;
+    private boolean applications;
+
+    public SocialNetwork() {
+        super();
+        usersAmount = 0;
+        applications = false;
+    }
+
+    public SocialNetwork(String name, boolean advertising, int usersAmount, boolean applications) {
+        super(name, advertising);
+        this.usersAmount = usersAmount;
+        this.applications = applications;
+    }
+
+    public SocialNetwork(String name, boolean advertising, int usersAmount) {
+        super(name, advertising);
+        this.usersAmount = usersAmount;
+    }
+
+    public SocialNetwork(SocialNetwork socialNetwork) {
+        super(socialNetwork);
+        usersAmount = socialNetwork.usersAmount;
+        applications = socialNetwork.applications;
+    }
+
+    public int getUsersAmount() {
+        return usersAmount;
+    }
+
+    public boolean getApplications() {
+        return applications;
+    }
+
+    public void setUsersAmount(int usersAmount) {
+        this.usersAmount = usersAmount;
+    }
+
+    public void setApplications(boolean applications) {
+        this.applications = applications;
+    }
+
+    @Override
+    public int calculatePopularity() {
+        return usersAmount + (applications ? 100 : 0);
+    }
 }
