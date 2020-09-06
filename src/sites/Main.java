1a7b3ed4af6ac112e701e26a83a4a435507c8b87
Кирилл Курко <kirillkurko2609@gmail.com>
2020-09-06 22:02:43 +0300

    Main class implemented

--- src/sites/Main.java
+++ src/sites/Main.java
@@ -1,8 +1,21 @@
 package sites;
 
+import sites.helperClasses.WriterInfo;
+import sites.siteClasses.EducationalSite;
+import sites.siteClasses.InformationSite;
+import sites.siteClasses.SocialNetwork;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        SocialNetwork socialNetwork = new SocialNetwork("Telegram", false, 50_000_000, false);
+        InformationSite informationSite = new InformationSite("NYTimes", true, 10_000, 10);
+        EducationalSite educationalSite = new EducationalSite("Udemy", false, "Programming");
+
+        WriterInfo.printSocialNetworkInformation(socialNetwork);
+        System.out.println();
+        WriterInfo.printInformationSiteInformation(informationSite);
+        System.out.println();
+        WriterInfo.printEducationalSiteInformation(educationalSite);
     }
 }
